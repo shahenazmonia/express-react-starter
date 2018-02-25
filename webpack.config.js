4860a84d0178516e0bb7fb81a45d75f90006606e
shahenaz Monia <shahy.m.93@gmail.com>
2018-02-25 13:07:43 +0200

    create starter for express-react

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const common= {
+  entry: {
+    bundle: ['./src/fronEnd/index.jsx']
+  },
+  output: {
+    path: __dirname + '/public/js',
+    filename: '[name].js',
+    publicPath: 'public'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$|\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    },
+    {
+      test: /\.css$/,
+      loader: ['style-loader', 'css-loader']
+    }
+    ]
+  },
+};
+const development = {
+  devtool:'inline-source-map'
+};
+const production = {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        compress: {
+          warnings: false,
+          screw_ie8: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+        }
+      }
+    })
+  ],
+};
+module.exports =
+  Object.assign(
+    common,
+    process.env.NODE_ENV === 'production'
+      ? production
+      : development
+  );
